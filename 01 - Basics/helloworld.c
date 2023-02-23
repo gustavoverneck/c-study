57f3e0db2b88006b7f3cb6fd708e767363c8ce7c
Gustavo A. Verneck <gustavoverneck@gmail.com>
2023-02-23 17:18:22 -0300

    Create helloworld.c

---
+++ 01 - Basics/helloworld.c
@@ -0,0 +1,9 @@
+// <stdio.h> is a labrary that stands for Standard Input Output
+// for more info about it see https://www.tutorialspoint.com/header-files-stdio-h-and-stdlib-h-in-c
+#include <stdio.h>
+
+int main() {
+  printf("Hello World");
+}
+
+return 0;
